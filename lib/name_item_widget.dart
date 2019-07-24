2e5058ec9102c317517e0c43d812142693f46f91
holyhost <2024212728@qq.com>
2019-07-24 10:56:57 +0800

    ❤ flutter

--- lib/name_item_widget.dart
+++ lib/name_item_widget.dart
@@ -10,22 +10,38 @@ class NameItemWidget extends StatefulWidget {
 }
 
 class _NameItemWidgetState extends State<NameItemWidget> {
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
+
       title: _buildItem(widget.bean),
     );
   }
 
   Widget _buildItem(NameBean bean){
     if(bean.children.isEmpty){
-      return ListTile(title: Text(bean.name),);
+      return ListTile(
+        title: Text(bean.name),
+        onTap: (){
+          _showSeletedName(bean.name);
+        },
+      );
     }
     return ExpansionTile(
       key: PageStorageKey<NameBean>(bean),
       title: Text(bean.name),
       children: bean.children.map<Widget>(_buildItem).toList(),
+      leading: CircleAvatar(
+        backgroundColor: Colors.green,
+        child: Text(bean.name.substring(0,1),style: TextStyle(color: Colors.white),),
+      ),
     );
   }
 
+
+  _showSeletedName(String name){
+
+    Scaffold.of(context).showSnackBar(SnackBar(content: Text("选择的是："+name)));
+  }
 }
